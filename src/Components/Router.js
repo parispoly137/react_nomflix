6ac49a82ca86d8ad323d1db47f49e1debc3f67c1
parispoly137 <tkdgns6578@naver.com>
2021-03-31 10:07:29 +0900

    #2.2 React Router part Two
    - export default 오류 해결 (함수명 export)
    - Switch ... 2개 Route 동시 렌더링
    - Header Route 추가
    - Redirect 추가

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,13 +1,19 @@
 import React from "react";
-import { HashRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom";
 import Home from "Routes/Home";
 import TV from "Routes/TV";
 import Search from "Routes/Search";
 
-export default () => (
+const router = () => (
   <Router>
-    <Route path="/" exact component={Home} />
-    <Route path="/tv" exact component={TV} />
-    <Route path="/search" exact component={Search} />
+    <Switch>
+      <Route path="/" exact component={Home} />
+      <Route path="/tv" exact component={TV} />
+      <Route path="/tv/popular" render={() => <h1> Popular</h1>} />
+      <Route path="/search" component={Search} />
+      <Redirect from="*" to="/" />
+    </Switch>
   </Router>
 );
+
+export default router;
