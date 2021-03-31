ce346aebf06e621f4a378aef33c73a77e2c0e027
parispoly137 <tkdgns6578@naver.com>
2021-03-31 09:20:59 +0900

    #2.1 React Router Part One
    - Hash Router 학습
    - env 파일 삭제 후 jsconfig.json 파일 생성 ~ 기본 경로 설정
    - .gitignore 파일 추가

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,7 +1,14 @@
-function App() {
-  return (
-    <div className="App" />
-  );
+import React, { Component } from "react";
+import Router from "Components/Router";
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <Router></Router>
+      </>
+    );
+  }
 }
 
 export default App;
